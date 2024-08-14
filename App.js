30991587a4e74c629553344550caeb149f7f0742
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-08-14 10:52:11 +0530

    setup the react native

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { View, Text, StyleSheet } from "react-native";
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>hi this is react native</Text>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2c3e50",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    margin: 80,
+  },
+  text: {
+    backgroundColor: "#27ae60",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
