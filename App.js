e8c9f2e1c888eee1443109f7c6619cf3bdb7822e
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-08-14 11:23:32 +0530

    used all images type

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
-import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { View, Image, StyleSheet, ImageBackground, Text } from "react-native";
+
+const logo = require("./assets/adaptive-icon.png");
+
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>hi this is react native</Text>
+      <Image
+        source={logo}
+        style={{ width: 300, height: 200, padding: 20, margin: 20 }}
+      />
+      <Image
+        source={{
+          uri: "https://img.freepik.com/free-photo/mom-spending-time-with-kid-beach_23-2150924964.jpg?size=626&ext=jpg&ga=GA1.1.1139437995.1723613942",
+        }}
+        style={{ width: 300, height: 300, padding: 20, margin: 20 }}
+      />
+
+      <ImageBackground source={logo} style={{ width: 100, height: 100 }}>
+        <Text style={styles.text}> this is image background</Text>
+      </ImageBackground>
     </View>
   );
 };
@@ -11,14 +28,11 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#2c3e50",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-    margin: 80,
+    backgroundColor: "#8e44ad",
+    flex: 1,
   },
   text: {
-    backgroundColor: "#27ae60",
+    color: "#27ae60",
     alignItems: "center",
     justifyContent: "center",
   },
